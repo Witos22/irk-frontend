2930d069c8ff6ac0413f0b10ee5627b9e6185b2b
Witold <34158@uniwersytetkaliski.edu.pl>
2026-03-25 16:14:36 +0100

    Instalacja bibliotek, realizacja formularza rejestracji

---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,67 @@
+import {useState} from 'react';
+
+function RegisterForm(){
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: '',
+        passwordHash: ''
+    });
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log("Wysyłanie danych do Springa...", formData);
+
+        try {
+            const response = await fetch('http://localhost:8081/api/candidates/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData)
+            });
+
+            if (response.ok){
+                const savedCandidate = await response.json();
+                console.log("Zapisano dane kandydata w bazie", savedCandidate);
+                alert("Zapisano dane kandydata w bazie");
+            } else {
+                console.error("Odrzucono. kod błędu:", response.status);
+                alert("Błąd! Sprawdź konsolę.");
+        }
+    } catch (error){
+        console.error("Błąd połączenia ze springiem", error);
+    }
+    };
+
+    return (
+        <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', width: '300px', gap: '10px' }}>
+            <label>Imię:</label>
+            <input type="text" name="firstName" value={formData.firstName} onChange={handleChange} required />
+
+            <label>Nazwisko:</label>
+            <input type="text" name="lastName" value={formData.lastName} onChange={handleChange} required />
+
+            <label>Email:</label>
+            <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+
+            <label>Numer telefonu:</label>
+            <input type="tel" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} required />
+
+            <label>Hasło:</label>
+            <input type="password" name="passwordHash" value={formData.passwordHash} onChange={handleChange} required />
+
+            <button type="submit" style={{ marginTop: '15px', padding: '10px' }}>Zarejestruj się</button>
+        </form>
+    );
+}
+
+export default RegisterForm;
